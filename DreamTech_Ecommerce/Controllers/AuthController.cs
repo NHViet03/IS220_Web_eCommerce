9eb1c9affba039d752228b8a145d0558557a67a1
vietlvq260 <vietlvq2609@gmail.com>
2023-11-16 01:46:13 +0700

    initiate SwaggerUI

--- DreamTech_Ecommerce/Controllers/AuthController.cs
+++ DreamTech_Ecommerce/Controllers/AuthController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace DreamTech_Ecommerce.Controllers
--- DreamTech_Ecommerce/Program.cs
+++ DreamTech_Ecommerce/Program.cs
@@ -3,13 +3,33 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Authorization;
-using System.Security.Cryptography;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "ToDo API",
+        Description = "An ASP.NET Core Web API for managing ToDo items",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+});
 builder.Services.AddDbContext<DreamAppContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase"));
@@ -45,7 +65,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-
 }
 
 app.UseStaticFiles();
@@ -61,5 +80,12 @@ app.MapControllerRoute(
 app.MapGet("/test", () => "Success Authorized").RequireAuthorization();
 
 app.MapFallbackToFile("index.html"); ;
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 app.Run();
