6c3ec1eff290c8a4839be3a3b3b17cbc15e17937
vietlvq260 <vietlvq2609@gmail.com>
2023-11-15 22:22:21 +0700

    add logintoken

---
+++ DreamTech_Ecommerce/Models/LoginToken.cs
@@ -0,0 +1,9 @@
+﻿namespace DreamTech_Ecommerce.Models
+{
+    public class LoginToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
